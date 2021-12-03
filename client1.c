ce13d48bc66cc28c1ab00887661faac83a9678ef
Determinant179 <travkinpa@mail.ru>
2021-12-03 16:40:57 +0300

    Add client 1 functionality

--- client1.c
+++ client1.c
@@ -5,7 +5,30 @@
 #include <unistd.h>
 #include <string.h>
 
-int main() {
+/*
+КЛИЕНТ 1
+    Записать в разделяемую область памяти идентификаторы всех процессов, 
+    приоритет которых больше 25, а также время работы этих процессов. 
+    Распечатать ответ сервера.
+*/
+
+struct sembuf waiting = {0, -1, 0};
+struct sembuf notify = {0, 2, 0};
+
+int main()
+{
+
+    FILE *fp;
+    if ((fp = popen("ps -eo pid,ni,time | awk \'{if ( $2 > 10 ) print }\'", 'r')) == NULL)
+    {
+        fprintf(stderr, "\nCLIENT 1:\nError while popen\n");
+        return 3;
+    }
+
+    char output[2048];
+    fgets(output, 2048, fp);
+
+    printf("%s", output);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -3,17 +3,25 @@
 #include <sys/sem.h>
 #include <stdlib.h>
 #include <string.h>
+
 /*
- * Вариант №20
- * Сервер.
- *  Создать разделяемую область памяти (РОП) и набор семафоров (НС).
- *      От поступивших в РОП от клиентов сообщений, сформировать информацию о количестве всех файлов,
- *          содержащихся в подкаталогах текущего каталога и вывести её на печать,
- *          а также вывести значение идентификатора.
- *  Удалить РОП и НС.
- */
-#define SHM_KEY 3
-#define SEM_KEY 4
+СЕРВЕР 
+    Создать разделяемую область памяти (РОП) и набор семафоров(НС). 
+    Ждать сообщений клиентов. При поступлении сообщения от 1-ого клиента, 
+    обработать его и переслать абоненту имена «родителей» этих процессов. 
+    Распечатать показания времени, а также содержимое сообщения, 
+    полученное от 2-ого клиента. Удалить РОП и НС.
+
+КЛИЕНТ 1
+    Записать в разделяемую область памяти идентификаторы всех процессов, 
+    приоритет которых больше 25, а также время работы этих процессов. 
+    Распечатать ответ сервера.
+
+КЛИЕНТ 2 
+    Записать в разделяемую область памяти время, 
+    когда какой-либо процесс последний раз выполнял операцию над 
+    семафорами набора. 
+*/
 
 struct sembuf unclock_first_client = {0, 1, 0};
 struct sembuf wait_first_client = {0, -2, 0};
@@ -23,25 +31,50 @@ struct sembuf wait_second_client = {0, -4, 0};
 int main()
 {
 
-    // Создать разделяемую область памяти (РОП)
-    int fd_shm = shmget(SHM_KEY, 2048, IPC_CREAT | IPC_EXCL | 0664);
+    // Создание РОП
+    int fd_shm = shmget(3, 2048, IPC_CREAT | IPC_EXCL | 0664);
     if (fd_shm == -1)
     {
-        fprintf(stderr, "server - error - shared memory creation\n");
+        fprintf(stderr, "\nSERVER:\nError while ShM creation\n");
         return 1;
     }
-    printf("server - create rop - ok\n");
+    printf("\nSERVER:\nSnM was created\n");
 
-    // Создать набор семафоров (НС)
-    int fd_sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0664);
+    // Создание семафоров
+    int fd_sem = semget(4, 1, IPC_CREAT | IPC_EXCL | 0664);
     if (fd_sem == -1)
     {
-        fprintf(stderr, "server - error - sem creation\n");
+        fprintf(stderr, "\nSERVER:\nError while semaphore set creation\n");
         return 2;
     }
-    printf("server - create ns - ok\n");
+    printf("\nSERVER:\nSemaphore set was created\n");
 
+    // Добавление РОП
+    char *addr = shmat(fd_shm, 0, 0);
+    if (addr == (char *)-1)
+    {
+        fprintf(stderr, "\nSERVER:\nError while  ShM adding\n");
+    }
 
+    // Разблокирование 1-ого клиента
+    semop(fd_sem, &unclock_first_client, 1);
+    // Ожидание 1-ого клиента
+    semop(fd_sem, &wait_first_client, 1);
+
+    char answer1[2048];
+    strcpy(answer1, addr);
+    printf("\nSERVER:\n Message from client 1: \"%s\"\n", answer1);
+
+    // Разблокирование 2-ого клиента
+    semop(fd_sem, &unclock_second_client, 1);
+    // Ожидание 2-ого клиента
+    semop(fd_sem, &wait_second_client, 1);
+
+    char answer2[2048];
+    strcpy(answer2, addr);
+    printf("\nSERVER:\n Message from client 2: \"%s\"\n", answer2);
+
+    shmdt(addr);
     shmctl(fd_shm, IPC_RMID, 0);
     semctl(fd_sem, 0, IPC_RMID);
 
