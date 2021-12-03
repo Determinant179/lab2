fc76dcd590e73bb1dd8ce0ae7665518b62a2dca8
Determinant179 <travkinpa@mail.ru>
2021-12-03 19:19:13 +0300

    Add client 2 functionality

--- client2.c
+++ client2.c
@@ -4,15 +4,73 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 /*
-КЛИЕНТ 2 
-    Записать в разделяемую область памяти время, 
-    когда какой-либо процесс последний раз выполнял операцию над 
-    семафорами набора. 
+КЛИЕНТ 2
+    Записать в разделяемую область памяти время,
+    когда какой-либо процесс последний раз выполнял операцию над
+    семафорами набора.
 */
 
-int main() {
+struct sembuf waiting = {0, -3, 0};
+struct sembuf notify = {0, 4, 0};
+
+int main()
+{
+
+    // Получение семафоров
+    int fd_sem = -1;
+    while (fd_sem == -1)
+    {
+        fd_sem = semget(4, 0, 0);
+        sleep(1);
+    }
+    semop(fd_sem, &waiting, 1);
+
+    // Получение РОП
+    int fd_shm = -1;
+    while (fd_shm == -1)
+    {
+        fd_shm = shmget(3, 0, 0);
+        sleep(1);
+    }
+
+    // Добавление РОП
+    char *addr = shmat(fd_shm, 0, 0);
+    if (addr == (char *)-1)
+    {
+        fprintf(stderr, "shared memory adding ERROR\n");
+    }
+
+    // Ожидание сервера
+
+    struct semid_ds semid_ds;
+    int rtrn, semid;
+
+    union semun
+    {
+        int val;
+        struct semid_ds *buf;
+        short array[25];
+
+    } arg;
+
+    // Инициализация указателя на структуру данных
+    arg.buf = &semid_ds;
+
+    rtrn = semctl(fd_sem, 0, GETPID);
+    char output[2048];
+    strcpy(output, "Last operation was performed at ");
+
+    time(&arg.buf->sem_otime);
+    strcat(output, ctime(&arg.buf->sem_otime));
+
+
+    strcpy(addr, output);
+
+    semop(fd_sem, &notify, 1);
+    shmdt(addr);
 
     return 0;
 }
