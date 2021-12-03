8aa812cd22f2e592f000a90ccd297834bac7c92b
Determinant179 <travkinpa@mail.ru>
2021-12-03 17:17:45 +0300

    Add client 1 functionality

--- client1.c
+++ client1.c
@@ -19,16 +19,58 @@ int main()
 {
 
     FILE *fp;
-    if ((fp = popen("ps -eo pid,ni,time | awk \'{if ( $2 > 10 ) print }\'", 'r')) == NULL)
+    if ((fp = popen("ps -eo pid,ni,time | awk '{if ( $2 > 10 ) print }' | tail +2", "r")) == NULL)
     {
         fprintf(stderr, "\nCLIENT 1:\nError while popen\n");
         return 3;
     }
 
     char output[2048];
-    fgets(output, 2048, fp);
+    char line[2048];
+
+    // Избавдяемся от мусора в начале строки (\0)
+    strcpy(output, "");
+
+    int i = 0;
+    while (1)
+    {
+        if (fgets(line, 2048, fp) == NULL)
+            break;
+        i++;
+        strcat(output, line);
+    }
 
     printf("%s", output);
+
+    // Получение семафоров
+    int fd_sem = -1;
+    while (fd_sem == -1) {
+        fd_sem = semget(3, 0, 0);
+        sleep(1);
+    }
+    semop(fd_sem, &waiting, 1);
+
+    // Получение РОП
+    int fd_shm = -1;
+    while (fd_shm == -1){
+        fd_shm = shmget(4, 0, 0);
+        sleep(1);
+    }
+
+    // Добавление РОП
+    char * addr = shmat(fd_shm, 0, 0);
+    if (addr == (char * ) -1)
+    {
+        fprintf(stderr, "\nCLIENT 1:\nError while ShM adding\n");
+    }
+
+    // Ожидание сервера
+
+    strcpy(addr, output);
+
+    semop(fd_sem, &notify, 1);
+    shmdt(addr);
+
 
     return 0;
 }
